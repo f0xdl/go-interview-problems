0dffa9fdb94c973441764613ce61272af6f92de9
f0xdl <f0xdl@proton.me>
2025-12-07 19:26:32 +0100

    resolve task1

--- 01-first-successful-key-lookup/task.go
+++ 01-first-successful-key-lookup/task.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 )
 
 type Getter interface {
@@ -12,5 +13,45 @@ type Getter interface {
 // Returns the first successful response.
 // If all requests fail, returns an error.
 func Get(ctx context.Context, getter Getter, addresses []string, key string) (string, error) {
-	return "", nil
+	if len(addresses) == 0 {
+		return "", nil
+	}
+
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	resCh := make(chan string)
+	errCh := make(chan error, len(addresses))
+
+	for _, address := range addresses {
+		go worker(ctx, getter, address, key, resCh, errCh)
+	}
+
+	errs := []error{}
+	for {
+		select {
+		case r := <-resCh:
+			cancel()
+			return r, nil
+		case err := <-errCh:
+			errs = append(errs, err)
+			if len(errs) == len(addresses) {
+				return "", errors.Join(errs...)
+			}
+		case <-ctx.Done():
+			return "", ctx.Err()
+		}
+	}
+}
+
+func worker(ctx context.Context, getter Getter, address, key string, result chan<- string, errs chan<- error) {
+	res, err := getter.Get(ctx, address, key)
+	if err != nil {
+		errs <- err
+	} else {
+		select {
+		case result <- res:
+		default:
+		}
+	}
 }
