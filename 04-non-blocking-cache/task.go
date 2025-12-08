baffd926c61c45406684932a0f8981eaf496f83c
f0xdl <f0xdl@proton.me>
2025-12-08 03:02:27 +0100

    resolve task4,task5

--- 04-non-blocking-cache/task.go
+++ 04-non-blocking-cache/task.go
@@ -1,22 +1,78 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
 type Client interface {
 	Get(address string) (string, error)
 }
 
 type Cache struct {
 	client Client
-	// You can add new fields if needed
+	// cache  sync.Map - alternative
+	cache      map[string]*Response
+	processing map[string]*sync.WaitGroup
+	mu         sync.Mutex
+}
+
+type Response struct {
+	Value string
+	Err   error
 }
 
 // Don't update signature of NewCache
 func NewCache(client Client) *Cache {
-	// TODO: Implement
-	return &Cache{client: client}
+	return &Cache{
+		client:     client,
+		mu:         sync.Mutex{},
+		cache:      map[string]*Response{},
+		processing: map[string]*sync.WaitGroup{},
+	}
 }
 
 // Cache Client.Get result
 func (c *Cache) Get(address string) (string, error) {
-	// TODO: Implement. Right now it doesn't cache
-	return c.client.Get(address)
+	c.mu.Lock()
+
+	if cache, ok := c.cache[address]; ok {
+		c.mu.Unlock()
+		return cache.Value, cache.Err
+	}
+
+	if wg, ok := c.processing[address]; ok {
+		c.mu.Unlock()
+		wg.Wait()
+
+		c.mu.Lock()
+		defer c.mu.Unlock()
+		cache := c.cache[address]
+		return cache.Value, cache.Err
+	}
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	c.processing[address] = wg
+	c.mu.Unlock()
+
+	resp := c.processAddress(address)
+
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.cache[address] = resp
+	delete(c.processing, address)
+	wg.Done()
+	return resp.Value, resp.Err
+}
+
+func (c *Cache) processAddress(address string) *Response {
+	resp := &Response{}
+	defer func() {
+		if r := recover(); r != nil {
+			resp.Err = fmt.Errorf("panic: %v", r)
+		}
+	}()
+	resp.Value, resp.Err = c.client.Get(address)
+	return resp
 }
--- 05-costly-connections-with-unsafe-storage/task.go
+++ 05-costly-connections-with-unsafe-storage/task.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"sync"
+)
+
 type Connection interface {
 	// Need call Connect before Send
 	// Take time to connect
@@ -27,5 +33,61 @@ type Saver interface {
 // SendAndSave should send all requests concurrently using at most `maxConn` simultaneous connections.
 // Responses must be saved using Saver.Save.
 // Be careful: Saver.Save is not safe for concurrent use.
-func SendAndSave(creator ConnectionCreator, saver Saver, requests []string, maxConn int) {
+func SendAndSave(creator ConnectionCreator, saver Saver, requests []string, maxConn int) { //+return error?
+	count := len(requests)
+	requestCh := make(chan string, count)
+	resultCh := make(chan string, count)
+	errCh := make(chan error, count)
+
+	//request gen
+	go func() {
+		for _, req := range requests {
+			requestCh <- req
+		}
+		close(requestCh)
+	}()
+
+	//processing
+	wg := &sync.WaitGroup{}
+	for id := range maxConn {
+		conn, err := creator.NewConnection()
+		if err != nil {
+			errCh <- err
+			continue
+		}
+		wg.Add(1)
+		go worker(id, wg, conn, requestCh, resultCh, errCh)
+	}
+	wg.Wait()
+	close(resultCh)
+	close(errCh)
+
+	//save and print
+	for res := range resultCh {
+		saver.Save(res)
+	}
+	for err := range errCh {
+		log.Println(err)
+	}
+}
+
+func worker(id int, wg *sync.WaitGroup, conn Connection, requests <-chan string, result chan<- string, errs chan<- error) {
+	defer wg.Done()
+	defer func() {
+		if r := recover(); r != nil {
+			errs <- fmt.Errorf("worker %d: panic - %v", id, r)
+		}
+	}()
+
+	conn.Connect()
+	defer conn.Disconnect()
+
+	for req := range requests {
+		r, err := conn.Send(req)
+		if err != nil {
+			errs <- err
+		} else {
+			result <- r
+		}
+	}
 }
